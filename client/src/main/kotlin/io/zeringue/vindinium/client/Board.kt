d1ef75a58997f897eedb63f1921da7351c86fb91
John Zeringue <john.joseph.zeringue@gmail.com>
2016-05-25 01:37:58 -0400

    Added `Board#contains`
    Added `Board#contains`, which checks whether or not a position is within
    the board.

--- client/src/main/kotlin/io/zeringue/vindinium/client/Board.kt
+++ client/src/main/kotlin/io/zeringue/vindinium/client/Board.kt
@@ -6,6 +6,10 @@ data class Board(
         @Key("size") val size: Int,
         @Key("tiles") val tiles: String) {
 
+    operator fun contains(pos: Position): Boolean {
+        return pos.run { x >= 0 && x < size && y >= 0 && y < size }
+    }
+
     operator fun get(pos: Position) = get(pos.x, pos.y)
 
     operator fun get(x: Int, y: Int): String {
